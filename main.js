3be1d1cf39c14b623628b2f3d91f6a12d1fc55ca
Some One <1234abcdcba4321@users.noreply.github.com>
2015-07-20 14:57:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function ancientcost(ancient,curlvl,goallvl){
 for (i=curlvl+1;i<=goallvl;i++){
 var cost+=Math.ceil(i^ancient.scale)
-}}
+}
+ if(ancient="kumawakaru"){cost*=10} 
+ return(cost);
+}
+
+function ancientpwr(){}
+
+function list(){}
